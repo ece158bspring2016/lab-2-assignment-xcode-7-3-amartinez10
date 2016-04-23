94b9c1d4ab5150700d814886b9daaa8d8e7cfc05
Allan Martinez <alm050@ucsd.edu>
2016-04-22 23:01:40 -0700

    Lab 1 attempt to submit

---
+++ Lab1/Player.swift
@@ -0,0 +1,21 @@
+//
+//  Player.swift
+//  Lab1
+//
+//  Created by Allan Martinez on 4/22/16.
+//  Copyright © 2016 Allan Martinez. All rights reserved.
+//
+
+import UIKit
+
+struct Player {
+    var name: String?
+    var game: String?
+    var rating: Int
+    
+    init(name: String?, game: String?, rating: Int) {
+        self.name = name
+        self.game = game
+        self.rating = rating
+    }
+}
---
+++ Lab1/SampleData.swift
@@ -0,0 +1,16 @@
+//
+//  SampleData.swift
+//  Lab1
+//
+//  Created by Allan Martinez on 4/22/16.
+//  Copyright © 2016 Allan Martinez. All rights reserved.
+//
+
+import Foundation
+
+//Set up sample data
+
+let playersData = [
+    Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
